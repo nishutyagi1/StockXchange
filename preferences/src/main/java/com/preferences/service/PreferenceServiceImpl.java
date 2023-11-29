9ff0ab8e5f15cb4fd50e9abeb087ea51963542ec
Nishu Tyagi <nishu.t@HCL.COM>
2023-11-29 10:32:17 -0800

    Record check before updating BD

--- preferences/src/main/java/com/preferences/service/PreferenceServiceImpl.java
+++ preferences/src/main/java/com/preferences/service/PreferenceServiceImpl.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.preferences.models.Preference;
 import com.preferences.repository.PreferenceRepository;
+import java.util.Objects;
+import java.util.Optional;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,12 @@ public class PreferenceServiceImpl implements PreferenceService {
   @Override
   public void save(ConsumerRecord<String, String> record) throws JsonProcessingException {
     Preference preference = objectMapper.readValue(record.value(), Preference.class);
-    preferenceRepository.save(preference);
+    String prefId = preference.getPrefId();
+    if(!Objects.nonNull(preferenceRepository.findById(prefId))) {
+      preferenceRepository.save(preference);
+    }
+    else {
+      System.out.println("Record exists");
+    }
   }
 }
