a8bba129c5e84a8823af9e7514690ebe0f35f43c
nishutyagi1 <147661446+nishutyagi1@users.noreply.github.com>
2023-12-05 14:15:15 -0800

    Update StockServiceImpl.java

--- customer/src/main/java/com/customer/service/StockServiceImpl.java
+++ customer/src/main/java/com/customer/service/StockServiceImpl.java
@@ -6,10 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Service
 public class StockServiceImpl implements StockService {
 
   @Autowired
